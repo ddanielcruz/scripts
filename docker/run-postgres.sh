9c7e923c78b52bacab5697d983827c2f19cef414
Daniel Cruz <danielcunha54@gmail.com>
2022-05-18 10:04:09 -0300

    Load environment variables and update restart clause

--- docker/run-postgres.sh
+++ docker/run-postgres.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+source ./.postgres.env;
 docker image pull postgres:14-alpine;
-docker container run -d --name postgres --restart always -p 5432:5432 -v postgres-data:/var/lib/postgresql/data -e POSTGRES_PASSWORD="$DB_PASS" postgres:14-alpine;
+docker container run -d --name postgres --restart unless-stopped -p 5432:5432 -v postgres-data:/var/lib/postgresql/data -e POSTGRES_PASSWORD="$DB_PASS" postgres:14-alpine;
--- docker/run-redis.sh
+++ docker/run-redis.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+source ./.redis.env;
 docker image pull bitnami/redis:6.2;
-docker container run -d --name redis --restart always -p 6379:6379 -e ALLOW_EMPTY_PASSWORD=no -e REDIS_PASSWORD="$REDIS_PASS" -v redis-data:/bitnami/redis/data bitnami/redis:6.2;
+docker container run -d --name redis --restart unless-stopped -p 6379:6379 -e ALLOW_EMPTY_PASSWORD=no -e REDIS_PASSWORD="$REDIS_PASS" -v redis-data:/bitnami/redis/data bitnami/redis:6.2;
